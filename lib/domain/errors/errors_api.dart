b2c7e93c5d33aa1f7a209942c1a9cf5751294c0d
Anton Labenskiy <al@decard.io>
2024-07-09 13:38:18 +0000

    Add errors api

---
+++ lib/domain/errors/errors_api.dart
@@ -0,0 +1,5 @@
+// An interface for errors functions.
+abstract class IError {
+  String getErrorMessage();
+  void clearErrors();
+}
---
+++ lib/extension/char_pointer.dart
@@ -0,0 +1,13 @@
+
+import 'dart:convert';
+import 'dart:ffi';
+
+// Extension method to convert a Pointer<Char> to a Dart string
+extension CharPointer on Pointer<Char> {
+  String toDartString({required int length}) {
+    final List<int> units = List<int>.generate(length, (int i) {
+      return elementAt(i).value;
+    });
+    return utf8.decode(units);
+  }
+}
