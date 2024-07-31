c5a1149221b3a7c0c7819c9750141d8f5241e337
Anton Labenskyi <al@decard.io>
2024-07-31 16:02:58 +0300

    Launch test from entrypoint

--- example/integration_test/document_integration_test.dart
+++ example/integration_test/document_integration_test.dart
@@ -13,9 +13,13 @@ import 'package:tagion_dart_api/error_message/error_message.dart';
 import 'package:tagion_dart_api/error_message/error_message_interface.dart';
 import 'package:tagion_dart_api/error_message/ffi/error_message_ffi.dart';
 import 'package:tagion_dart_api/pointer_manager/pointer_manager.dart';
-import 'package:tagion_dart_api/utils/ffi_library_util.dart';
 
 void main() {
+  final DynamicLibrary dyLib = DynamicLibrary.process();
+  documentIntegrationTests(dyLib);
+}
+
+void documentIntegrationTests(DynamicLibrary dyLib) {
   /// A real contract as a serialized HiBON.
   final hibonBuffer = Uint8List.fromList(base64Url.decode(
       '_QMBAiRABUhpUlBDAwIkWSEDGuaPS9u3r22WHd93J2uEquHBv_A4qE9g9ca5qxL5JrwCBCRtc2f9AhQCaWThzpPzBQEGbWV0aG9kBnN1Ym1pdAIGcGFyYW1z2wIBAiRAA1NTQwIJJGNvbnRyYWN0-QEBAiRAA1NNQwIDJGluJAMAACChE1h7ONUZg6hyDzwM1qqtf45FhgHKy8zWWgpN_sj2VwIEJHJ1br8BAQIkQANwYXkCBSR2YWxzrgECAABTAQIkQANUR04CAiRWChIBJICwr4yJoAEDAiRZIQLg1niES5tE941z3cyVO7ohtAZr4SAr11ZXFjMqRkXgzwkCJHTW8vHjpvar7ggDAiR4BMaIdRACAAFTAQIkQANUR04CAiRWChIBJICgt-yD_QADAiRZIQKYkUOPyywdDJ6G8OZ5eMLBKhvMhr4O3nuxfUiq1sO3BAkCJHSO6PTjpvar7ggDAiR4BPSQmA0CBiRzaWduc0QDAABAZRhWEMrAzAVqBJKa-pQIMf0O5PqDOLd62vzka6Z_MYaZq5rk39M47EdMeycnTNfGUot7Z1HdutgrNvcHme2KnAMFJHNpZ25AhgGz68Pq9LWPcnQPKeumtLYh0mGnnNsBk764abVvBYGthZDOYebP7zyz95wf-e2Kc7XqKOw3delXWcMwWXTEjA=='));
@@ -27,17 +31,13 @@ void main() {
   late final Basic basic;
   late final Document hibonDoc;
 
-  /// Library loading.
-  final DynamicLibrary library = FFILibraryUtil.load();
-
   /// FFI.
-  basicFfi = BasicFfi(library);
-  documentFfi = DocumentFfi(library);
-  errorMessageFfi = ErrorMessageFfi(library);
+  basicFfi = BasicFfi(dyLib);
+  documentFfi = DocumentFfi(dyLib);
+  errorMessageFfi = ErrorMessageFfi(dyLib);
 
   errorMessage = ErrorMessage(errorMessageFfi, const PointerManager());
   basic = Basic(basicFfi, const PointerManager(), errorMessage);
-  basic.startDRuntime();
   hibonDoc = Document(documentFfi, const PointerManager(), errorMessage, hibonBuffer);
 
   group('Document-DocumentFfi-DynamicLibrary Integration.', () {
@@ -137,7 +137,7 @@ void main() {
 
       const testIndex = 0;
       const dartIndexTestValue = '@oRNYezjVGYOocg88DNaqrX-ORYYBysvM1loKTf7I9lc=';
-      
+
       final element5 = inDoc.getElementByIndex(testIndex);
       Uint8List elementBinary = element5.getBinary();
 
