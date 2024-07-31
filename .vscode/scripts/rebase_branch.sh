31c048636cb427f7d6a24467a3b4259c58e3488a
Maksym Spiridonov <spiridonov.max.w@gmail.com>
2024-07-31 17:08:59 +0100

    fix to reckout to origin/main

--- .vscode/scripts/rebase_branch.sh
+++ .vscode/scripts/rebase_branch.sh
@@ -1,6 +1,6 @@
 read -p "Enter the name of the branch to rebase onto main: " branchName
 git pull
-git checkout main
+git checkout origin/main
 git rebase main  "$branchName"
 git push --force
 git pull
