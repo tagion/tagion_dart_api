58c2a177c3f4034ff2d0f9dd09300bea36730b1f
Anton Labenskyi <al@decard.io>
2024-07-31 12:13:27 +0300

    Add a document integration test

--- example/integration_test/document_integration_test.dart
+++ example/integration_test/document_integration_test.dart
@@ -1,46 +1,207 @@
 import 'dart:convert';
 import 'dart:ffi';
-import 'dart:io';
 import 'dart:typed_data';
 
 import 'package:flutter_test/flutter_test.dart';
+import 'package:tagion_dart_api/basic/basic.dart';
+import 'package:tagion_dart_api/basic/ffi/basic_ffi.dart';
 import 'package:tagion_dart_api/document/document.dart';
 import 'package:tagion_dart_api/document/ffi/document_ffi.dart';
+import 'package:tagion_dart_api/enums/document_error_code.dart';
 import 'package:tagion_dart_api/enums/document_text_format.dart';
 import 'package:tagion_dart_api/error_message/error_message.dart';
 import 'package:tagion_dart_api/error_message/error_message_interface.dart';
 import 'package:tagion_dart_api/error_message/ffi/error_message_ffi.dart';
 import 'package:tagion_dart_api/pointer_manager/pointer_manager.dart';
-import 'package:tagion_dart_api/pointer_manager/pointer_manager_interface.dart';
+import 'package:tagion_dart_api/utils/ffi_library_util.dart';
 
-/// TODO: Add a test.
 void main() {
+  /// A real contract as a serialized HiBON.
+  final hibonBuffer = Uint8List.fromList(base64Url.decode(
+      '_QMBAiRABUhpUlBDAwIkWSEDGuaPS9u3r22WHd93J2uEquHBv_A4qE9g9ca5qxL5JrwCBCRtc2f9AhQCaWThzpPzBQEGbWV0aG9kBnN1Ym1pdAIGcGFyYW1z2wIBAiRAA1NTQwIJJGNvbnRyYWN0-QEBAiRAA1NNQwIDJGluJAMAACChE1h7ONUZg6hyDzwM1qqtf45FhgHKy8zWWgpN_sj2VwIEJHJ1br8BAQIkQANwYXkCBSR2YWxzrgECAABTAQIkQANUR04CAiRWChIBJICwr4yJoAEDAiRZIQLg1niES5tE941z3cyVO7ohtAZr4SAr11ZXFjMqRkXgzwkCJHTW8vHjpvar7ggDAiR4BMaIdRACAAFTAQIkQANUR04CAiRWChIBJICgt-yD_QADAiRZIQKYkUOPyywdDJ6G8OZ5eMLBKhvMhr4O3nuxfUiq1sO3BAkCJHSO6PTjpvar7ggDAiR4BPSQmA0CBiRzaWduc0QDAABAZRhWEMrAzAVqBJKa-pQIMf0O5PqDOLd62vzka6Z_MYaZq5rk39M47EdMeycnTNfGUot7Z1HdutgrNvcHme2KnAMFJHNpZ25AhgGz68Pq9LWPcnQPKeumtLYh0mGnnNsBk764abVvBYGthZDOYebP7zyz95wf-e2Kc7XqKOw3delXWcMwWXTEjA=='));
+
+  late final BasicFfi basicFfi;
+  late final DocumentFfi documentFfi;
+  late final ErrorMessageFfi errorMessageFfi;
+  late final IErrorMessage errorMessage;
+  late final Basic basic;
+  late final Document hibonDoc;
+
+  /// Library loading.
+  final DynamicLibrary library = FFILibraryUtil.load();
+
+  /// FFI.
+  basicFfi = BasicFfi(library);
+  documentFfi = DocumentFfi(library);
+  errorMessageFfi = ErrorMessageFfi(library);
+
+  errorMessage = ErrorMessage(errorMessageFfi, const PointerManager());
+  basic = Basic(basicFfi, const PointerManager(), errorMessage);
+  basic.startDRuntime();
+  hibonDoc = Document(documentFfi, const PointerManager(), errorMessage, hibonBuffer);
+
   group('Document-DocumentFfi-DynamicLibrary Integration.', () {
-    final DynamicLibrary dyLib = Platform.isAndroid ? DynamicLibrary.open('libtauonapi.so') : DynamicLibrary.process();
+    test("Validate the document", () {
+      DocumentErrorCode errorCode = hibonDoc.validate();
+      expect(errorCode, DocumentErrorCode.none);
+    });
 
-    
-    final DocumentFfi documentFfi = DocumentFfi(dyLib);
-    final ErrorMessageFfi errorMessageFfi = ErrorMessageFfi(dyLib);
-    const IPointerManager pointerManager = PointerManager();
-    final IErrorMessage errorMessage = ErrorMessage(errorMessageFfi, pointerManager);
+    test("Document as string in different text formats", () {
+      String docAsJson = hibonDoc.getAsString(DocumentTextFormat.json);
+      expect(docAsJson, isNotEmpty);
+      String docAsPrettyJson = hibonDoc.getAsString(DocumentTextFormat.prettyJson);
+      expect(docAsPrettyJson, isNotEmpty);
+      String docAsBase64 = hibonDoc.getAsString(DocumentTextFormat.base64);
+      expect(docAsBase64, isNotEmpty);
+      String docAsHex = hibonDoc.getAsString(DocumentTextFormat.hex);
+      expect(docAsHex, isNotEmpty);
+    });
 
-    test('Read a Document', () {
-      final documentData = Uint8List.fromList(base64Url.decode(
-          '_QMBAiRABUhpUlBDAwIkWSEDGuaPS9u3r22WHd93J2uEquHBv_A4qE9g9ca5qxL5JrwCBCRtc2f9AhQCaWThzpPzBQEGbWV0aG9kBnN1Ym1pdAIGcGFyYW1z2wIBAiRAA1NTQwIJJGNvbnRyYWN0-QEBAiRAA1NNQwIDJGluJAMAACChE1h7ONUZg6hyDzwM1qqtf45FhgHKy8zWWgpN_sj2VwIEJHJ1br8BAQIkQANwYXkCBSR2YWxzrgECAABTAQIkQANUR04CAiRWChIBJICwr4yJoAEDAiRZIQLg1niES5tE941z3cyVO7ohtAZr4SAr11ZXFjMqRkXgzwkCJHTW8vHjpvar7ggDAiR4BMaIdRACAAFTAQIkQANUR04CAiRWChIBJICgt-yD_QADAiRZIQKYkUOPyywdDJ6G8OZ5eMLBKhvMhr4O3nuxfUiq1sO3BAkCJHSO6PTjpvar7ggDAiR4BPSQmA0CBiRzaWduc0QDAABAZRhWEMrAzAVqBJKa-pQIMf0O5PqDOLd62vzka6Z_MYaZq5rk39M47EdMeycnTNfGUot7Z1HdutgrNvcHme2KnAMFJHNpZ25AhgGz68Pq9LWPcnQPKeumtLYh0mGnnNsBk764abVvBYGthZDOYebP7zyz95wf-e2Kc7XqKOw3delXWcMwWXTEjA=='));
+    test("Get a document's record name", () {
+      String docRecName = hibonDoc.getRecordName();
+      expect(docRecName, isNotEmpty);
+    });
 
-      Document document = Document(
+    test("Get a document's version", () {
+      int docVersion = hibonDoc.getVersion();
+      expect(docVersion, 0);
+    });
+
+    test("Read the document", () {
+      /// "$@": "HiRPC"
+      const atKey = '\$@';
+      const typeTestValue = 'HiRPC';
+
+      final atElement = hibonDoc.getElementByKey(atKey);
+      String atResult = atElement.getString();
+      expect(atResult, typeTestValue);
+
+      /// "$Y": []
+      const yKey = '\$Y';
+      const pubKeyTestValue = '@Axrmj0vbt69tlh3fdydrhKrhwb_wOKhPYPXGuasS-Sa8';
+
+      final yElement = hibonDoc.getElementByKey(yKey);
+      Uint8List yElementBinary = yElement.getBinary();
+      String yResult = basic.encodeBase64Url(yElementBinary);
+      expect(yResult, pubKeyTestValue);
+
+      /// "$msg": {}
+      const testIndex = 0;
+      const dartIndexTestValue = '@oRNYezjVGYOocg88DNaqrX-ORYYBysvM1loKTf7I9lc=';
+
+      const msgKey = '\$msg';
+      final msgElement = hibonDoc.getElementByKey(msgKey);
+      final msgBuffer = msgElement.getSubDocument();
+      final msgDoc = Document(
         documentFfi,
-        pointerManager,
+        const PointerManager(),
         errorMessage,
-        documentData,
+        msgBuffer,
       );
 
-      // document.getElementByKey('key');
-      // document.getElementByIndex(index: 0);
-      // document.getRecordName();
-      // document.validate();
-      String docAsStr = document.getAsString(DocumentTextFormat.json);
-      print(docAsStr);
+      /// "id": []
+      const idTestValue = 1583671137;
+      final idElement = msgDoc.getElementByKey('id');
+      final idResult = idElement.getUint32();
+      expect(idResult, idTestValue);
+
+      /// "params": {}
+      const paramsKey = 'params';
+      final paramsElement = msgDoc.getElementByKey(paramsKey);
+      final paramsBuffer = paramsElement.getSubDocument();
+      final paramsDoc = Document(
+        documentFfi,
+        const PointerManager(),
+        errorMessage,
+        paramsBuffer,
+      );
+
+      /// "$contract": {}
+      const contractKey = '\$contract';
+      final contractElement = paramsDoc.getElementByKey(contractKey);
+      final contractBuffer = contractElement.getSubDocument();
+      final contractDoc = Document(
+        documentFfi,
+        const PointerManager(),
+        errorMessage,
+        contractBuffer,
+      );
+
+      /// "$in": []
+      const testKey4 = '\$in';
+      final element4 = contractDoc.getElementByKey(testKey4);
+      final inBuffer = element4.getSubDocument();
+      final inDoc = Document(
+        documentFfi,
+        const PointerManager(),
+        errorMessage,
+        inBuffer,
+      );
+
+      final element5 = inDoc.getElementByIndex(testIndex);
+      Uint8List elementBinary = element5.getBinary();
+
+      String dartIndexResult = basic.encodeBase64Url(elementBinary);
+      expect(dartIndexResult, dartIndexTestValue);
+
+      /// "$run" : {}
+      const runKey = '\$run';
+      final runElement = contractDoc.getElementByKey(runKey);
+      final runBuffer = runElement.getSubDocument();
+      final runDoc = Document(
+        documentFfi,
+        const PointerManager(),
+        errorMessage,
+        runBuffer,
+      );
+
+      /// "$vals": []
+      const valsKey = '\$vals';
+      final valsElement = runDoc.getElementByKey(valsKey);
+      final valsBuffer = valsElement.getSubDocument();
+      final valsDoc = Document(
+        documentFfi,
+        const PointerManager(),
+        errorMessage,
+        valsBuffer,
+      );
+
+      const valIndex = 0;
+      final valElement = valsDoc.getElementByIndex(valIndex);
+      final valBuffer = valElement.getSubDocument();
+      final valDoc = Document(
+        documentFfi,
+        const PointerManager(),
+        errorMessage,
+        valBuffer,
+      );
+
+      /// "$t": []
+      const tKey = '\$t';
+      const tTestValue = "2024-07-29T12:40:03.890415";
+
+      final tElement = valDoc.getElementByKey(tKey);
+      int resultTime = tElement.getTime();
+      Duration difference = DateTime.parse(tTestValue).difference(DateTime.utc(1, 1, 1));
+      expect(resultTime, difference.inMicroseconds * 10);
+
+      /// "$V": {}
+      const vKey = '\$V';
+      const vTestValue = 5500000000000;
+      final vElement = valDoc.getElementByKey(vKey);
+      final vBuffer = vElement.getSubDocument();
+      final vDoc = Document(
+        documentFfi,
+        const PointerManager(),
+        errorMessage,
+        vBuffer,
+      );
+
+      /// "$": []
+      const dolKey = '\$';
+      final dolElement = vDoc.getElementByKey(dolKey);
+      int vResult = dolElement.getInt64();
+      expect(vResult, vTestValue);
     });
   });
 }
