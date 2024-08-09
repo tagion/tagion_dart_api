330e2973c72b0308a9b1c64e0c4e14a4a74c754d
Anton Labenskyi <al@decard.io>
2024-08-09 12:06:54 +0300

    Add a SecureNet parameter

--- lib/hirpc/hirpc_interface.dart
+++ lib/hirpc/hirpc_interface.dart
@@ -1,9 +1,11 @@
 import 'dart:typed_data';
 
+import 'package:tagion_dart_api/crypto/secure_net_vault.dart';
+
 abstract interface class IHiRPC {
   /// Create a sender.
-  Uint8List createSender(String method, Uint8List param);
+  Uint8List createSender(String method, Uint8List docBuffer);
 
   /// Create a signed sender.
-  Uint8List createSignedSender(String method, Uint8List param, Uint8List deriver);
+  Uint8List createSignedSender(String method, SecureNetVault vault, Uint8List docBuffer, Uint8List deriver);
 }
