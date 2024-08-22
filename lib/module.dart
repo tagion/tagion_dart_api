dddd6b5725372480eaf98f2685125d35d186ea8f
Anton Labenskyi <al@decard.io>
2024-08-22 11:39:47 +0300

    Add a usage of IScope interface instead of its impl in the Module

--- lib/module.dart
+++ lib/module.dart
@@ -1,10 +1,11 @@
 import 'package:tagion_dart_api/error_message/error_message_interface.dart';
 import 'package:tagion_dart_api/scope.dart';
+import 'package:tagion_dart_api/scope_interface.dart';
 
 /// The [Module] is an abstract class.
 /// Contains a [Scope] object.
 abstract class Module {
-  final Scope scope;
+  final IScope scope;
 
   Module(IErrorMessage errorMessage) : scope = Scope(errorMessage);
 }
